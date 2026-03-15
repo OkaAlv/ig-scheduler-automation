ee403e1bf74d525103abc262424a8107bcca948b
OkaAlv <okaalv3@gmail.com>
2026-03-15 22:12:43 +0700

    Update Security

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,18 +2,17 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module'; // Untuk mengecek data user
-import { PrismaModule } from '../prisma/prisma.module'; // Untuk akses database
+import { UsersModule } from '../users/users.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
     UsersModule,
     PrismaModule,
-    // Konfigurasi Mesin Pembuat Tiket (JWT)
     JwtModule.register({
       global: true,
-      secret: 'RAHASIA_NEGARA_MEDAN_123', // Kunci rahasia untuk menyegel tiket (nanti bisa dipindah ke .env)
-      signOptions: { expiresIn: '1d' }, // Tiket otomatis hangus/expired dalam 1 hari
+      secret: process.env.JWT_SECRET as string, 
+      signOptions: { expiresIn: '1d' }, 
     }),
   ],
   controllers: [AuthController],
